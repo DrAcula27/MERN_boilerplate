fe91b0a2d30e0dff1345e1d71c426732ee12908b
Danielle <danielle@kendallspace.net>
2023-02-10 12:05:19 -0500

    create react app, setup express server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+// allow cross origin access
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// logs the different requests to the server
+app.use(logger("dev"));
+
+// parse stringified objects (JSON)
+app.use(express.json());
+
+// serve build folder
+app.use(express.static(path.join(__dirname, "build")));
+
+// ROUTES
+
+// catch-all route, must be last in route list
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// tell server where to listen. Must not be 3000 as React listens there.
+app.listen(5000, () => {
+  console.log(`Server is Listening on 5000`);
+});
