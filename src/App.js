f8adc55abf2fa7785a4020c56aac64ed10ad97a1
Danielle <danielle@kendallspace.net>
2023-02-23 16:55:16 -0500

    clean up boilerplate

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  // test routing
-  const testFunc = async () => {
-    const response = await fetch("/test_route");
-    console.log(response);
-  };
-  testFunc();
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
